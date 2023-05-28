3269c3d870c7dcf66d918728882f9a41cf10b0a2
Vincent <limvincent14@gmail.com>
2023-05-28 21:34:03 +0700

    tambah nama

--- app/games/page.jsx
+++ app/games/page.jsx
@@ -3,7 +3,7 @@ import LandingPageLayout from "../layouts/LandingPageLayout";
 export default function About() {
     return (
         <LandingPageLayout>
-            <h1>helo games</h1>;
+            <h1>halo everybody</h1>;
         </LandingPageLayout>
     );
 }
