c8e80220c8bfdb3741821ea3c150495b391cb2ba
Adanpradana <adanizkahariz@gmail.com>
2023-05-31 23:56:44 +0700

    rebuild project

--- app/page.js
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import LandingPageLayout from "./layouts/LandingPageLayout";
-
-export default function Home() {
-    return <LandingPageLayout>hello home</LandingPageLayout>;
-}
