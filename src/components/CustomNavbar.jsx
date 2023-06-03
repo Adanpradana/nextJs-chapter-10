ffa71f779e73ec47d4fb7e24071dc23b6f97506b
Adanpradana <adanizkahariz@gmail.com>
2023-06-03 07:45:52 +0700

    delete bugs

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,66 +1,56 @@
 import Link from "next/link";
 import Image from "next/image";
-import bg from "../../public/user.png"
-
+import { BiUser } from "react-icons/bi";
 export default function CustomNavbar() {
-    return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            />
-                        </svg>
-                    </label>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-                    >
-                        <li>
-                            <a>Item 1</a>
-                        </li>
-                        <li>
-                            <a>Item 3</a>
-                        </li>
-                    </ul>
-                </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <Link href={"/"}>Home</Link>
-                    </li>
-                    <li>
-                        <Link href={"/about"}>About</Link>
-                    </li>
-                    <li>
-                        <Link href={"/games"}>Games</Link>
-                    </li>
-                    <li>
-                        <Link href={"/contact"}>Contact</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <Link href={"/auth/login"}>
-                    <Image 
-                    style={{ width: 40, height: 40, marginRight: 10}}
-                    rounded
-                    src={bg} />
-                </Link>
-            </div>
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
         </div>
-    );
+        <Link className="btn btn-ghost normal-case text-xl" href={"/"}>
+          Alpha Centaury
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/games"}>Games</Link>
+          </li>
+          <li>
+            <Link href={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end text-white">
+        <Link href={"/auth/login"}>
+          <BiUser className="w-10 h-10" />
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -3,53 +3,9 @@ import LandingPage from "@/layouts/LandingPage";
 import styles from "../../pages/page.module.css";
 
 const inter = Inter({
-    subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export default function Games() {
-  return (
-    <LandingPage>
-            <main>
-            <div className="relative pt-16 pb-32 flex content-center items-center justify-center"
-                style={{minHeight: "75vh" }}>
-              <div className={`absolute top-0 w-full h-full bg-center bg-cover ${styles.BannerImage2}`}>
-                <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
-              </div>
-              <div className="container relative mx-auto">
-                  <div className="items-center flex flex-wrap">
-                    <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                      <div className="pr-12">
-                        <h1 className="text-white font-semibold text-5xl">
-                          Your story starts with us.
-                        </h1>
-                        <p className="mt-4 text-lg text-gray-300">
-                          This is a simple example of a Landing Page you can build
-                          using Tailwind Starter Kit. It features multiple CSS
-                          components based on the Tailwindcss design system.
-                        </p>
-                      </div>
-                    </div>
-
-                  </div>
-              </div>
-              <div>
-                <svg
-                  className="absolute bottom-0 overflow-hidden"
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                  version="1.1"
-                  viewBox="0 0 2560 100"
-                  x="0"
-                  y="0"
-                >
-                  <polygon
-                    className="text-gray-300 fill-current"
-                    points="2560 0 2560 100 0 100"
-                  ></polygon>
-                </svg>
-              </div>
-            </div>
-        </main>
-    </LandingPage>
-  )
+  return <LandingPage>test games</LandingPage>;
 }
